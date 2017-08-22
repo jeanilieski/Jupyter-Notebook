cb1aa5ae36fb75304c134379c7f2f571b79e64ab
Zan Ilieski <jeanilieski1@gmail.com>
2017-08-22 12:29:29 +0200

    Operational System

---
+++ OPERATIONAL SYSTEM.py
@@ -0,0 +1,36 @@
+#>cd %homepath%
+#C:Users\Olga and Jean>chdir
+#C:Users...Jean>pip install xlrd
+#C:Users...Jean>chdir
+#C:Users...Jean>python
+#           >>>import xlrd
+#...Jean>jupyter notebook to open jupyter
+
+#cd\   returnst to the root
+#C:\cd python27 -you enter the python27 folder
+#cd python27\Scripts    brings you to the file with the docs
+#C:\Program Files\MySQL\MySQL Server 5.7\bin -is where my MySQL is storred
+#bin> mysql -u root -p -h (eventually it could ask for host but here it doesn't)
+#mysql> exit
+#Ctrl+C stops the runing of the shell
+#systemsteuerung: PC-Info
+#just input %save my_useful_session 10-20 23 to save input lines 10 to 20 and 23 to my_useful_session.py.
+#IDLE uses the so called 'dataset' db and 'MySQLdb' while Jupyter 'sqlite3'
+
+json.dumps
+json data analysis with pandas
+https://www.youtube.com/watch?v=bPw7wk7Tcy8 #Read Write JSON with Python Pandas
+https://www.youtube.com/watch?v=Kf0q4Tf5M3c #Python Tutorial - 14. Working With JSON
+https://www.youtube.com/watch?v=XMjSGGej9y8 #Joining and Merging Dataframes - p.6
+
+
+
+
+##First go to the location of your conda.exe, for me it's:
+##
+##cd C:\Program Files\Anaconda3\Scripts
+##C:\Users\Olga and Jean\Anaconda2\Scripts
+##Then install the package:
+##
+##conda.exe install mysql-connector-python
+
