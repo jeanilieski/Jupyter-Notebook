d332c3472b128066d00d2d83a01cd7fe29c5d6ea
Zhan Ilieski <jeanilieski1@gmail.com>
2025-12-10 15:48:44 +0100

    Update 1c. Add more data from Excel into MySQL using Python.py

--- 1c. Add more data from Excel into MySQL using Python.py
+++ 1c. Add more data from Excel into MySQL using Python.py
@@ -8,7 +8,7 @@ book = xlrd.open_workbook("Employee_banc2.xlsx") #it was Employee_banc1 first
 sheet = book.sheet_by_name("Sheet1")
 
 # Establish a MySQL connection
-database = MySQLdb.connect (host="localhost", user = "root", passwd = "7b48i49x", db = "banc")
+database = MySQLdb.connect (host="localhost", user = "root", passwd = "7...x", db = "banc")
 
 # Get the cursor, which is used to traverse the database, line by line
 cursor = database.cursor()
@@ -54,3 +54,4 @@ rows = str(sheet.nrows)
 print "I just imported " + columns + " columns and " + rows + " rows to MySQL!"
 
 #check in db 'select * from employee_banc'
+
